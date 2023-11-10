b28fc51a1b80da2548a2a7982ad682ab32ad30c9
antidote <>
2023-11-10 16:53:42 +0900

    fix/MatchingServiceImpl 코드 리펙토링

--- matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/matching/entity/MatchingLog.java
+++ matchingService/src/main/java/com/nbcamp/gamematching/matchingservice/matching/entity/MatchingLog.java
@@ -1,5 +1,6 @@
 package com.nbcamp.gamematching.matchingservice.matching.entity;
 
+import com.nbcamp.gamematching.matchingservice.matching.dto.RequestMatching;
 import com.nbcamp.gamematching.matchingservice.member.entity.Member;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
@@ -10,7 +11,6 @@ import lombok.NoArgsConstructor;
 @Entity
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class MatchingLog {
-
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
@@ -18,12 +18,12 @@ public class MatchingLog {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id")
     private Member member;
+
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "resultMatching_id")
     private ResultMatching resultMatching;
 
     private Boolean evaluation = false;
-
 
     public MatchingLog(ResultMatching resultMatching, Member member) {
         this.resultMatching = resultMatching;
@@ -33,7 +33,6 @@ public class MatchingLog {
     public void changeEvaluation() {
         this.evaluation = true;
     }
-
 
     public String getMemberNickname() {
         return this.getMember().getProfile().getNickname();
